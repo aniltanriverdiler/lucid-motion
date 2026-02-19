2e0ba795456cf79741e97b51eb73d9faa5e15382
Anıl Tanrıverdiler <186810775+aniltanriverdiler@users.noreply.github.com>
2026-02-19 13:26:12 +0300

    feat: Added About.tsx component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,11 @@
 import Hero from "@/components/Hero";
+import About from "@/components/About";
 
 export default function Home() {
   return (
     <main className="relative min-h-screen w-screen overflow-x-hidden">
       <Hero />
-
-      <section className="z-0 min-h-screen bg-blue-500" />
+      <About />
     </main>
   );
 }
