419b8c23d2625513b08bd51a3902ac5cde4c7dce
Anıl Tanrıverdiler <186810775+aniltanriverdiler@users.noreply.github.com>
2026-02-20 11:05:10 +0300

    feat: Added Story.tsx component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,9 @@ import Hero from "@/components/Hero";
 import About from "@/components/About";
 import Navbar from "@/components/Navbar";
 import Features from "@/components/Features";
+import Story from "@/components/Story";
+import Contact from "@/components/Contact";
+import Footer from "@/components/Footer";
 
 export default function Home() {
   return (
@@ -10,6 +13,9 @@ export default function Home() {
       <Hero />
       <About />
       <Features />
+      <Story />
+      <Contact />
+      <Footer />
     </main>
   );
 }
---
+++ src/components/Story.tsx
@@ -0,0 +1,141 @@
+"use client";
+import gsap from "gsap";
+import { useRef } from "react";
+
+import Button from "./Button";
+import AnimatedTitle from "./AnimatedTitle";
+
+const FloatingImage = () => {
+  // Reference to the image element
+  const frameRef = useRef(null);
+
+  // Handles mouse movement over the image
+  const handleMouseMove = (e) => {
+    const { clientX, clientY } = e;
+    const element = frameRef.current;
+
+    if (!element) return;
+
+    // Get the dimensions of the image
+    const rect = element.getBoundingClientRect();
+    // Calculate the position of the mouse relative to the image
+    const xPos = clientX - rect.left;
+    const yPos = clientY - rect.top;
+
+    // Calculate the center of the image
+    const centerX = rect.width / 2;
+    const centerY = rect.height / 2;
+
+    // Calculate the rotation of the image
+    const rotateX = ((yPos - centerY) / centerY) * -10;
+    const rotateY = ((xPos - centerX) / centerX) * 10;
+
+    gsap.to(element, {
+      duration: 0.3,
+      rotateX,
+      rotateY,
+      transformPerspective: 500,
+      ease: "power1.inOut",
+    });
+  };
+
+  // Handles mouse leave event
+  const handleMouseLeave = () => {
+    const element = frameRef.current;
+
+    if (element) {
+      gsap.to(element, {
+        duration: 0.3,
+        rotateX: 0,
+        rotateY: 0,
+        ease: "power1.inOut",
+      });
+    }
+  };
+
+  return (
+    <div id="story" className="min-h-dvh w-screen bg-black text-blue-50">
+      <div className="flex size-full flex-col items-center py-10 pb-24">
+        {/* Title Section */}
+        <p className="font-general text-sm uppercase md:text-[10px]">
+          the multiversal ip world
+        </p>
+
+        {/* Title Animation Section */}
+        <div className="relative size-full">
+          <AnimatedTitle
+            title="the st<b>o</b>ry of <br /> a hidden real<b>m</b>"
+            containerClass="mt-5 pointer-events-none mix-blend-difference relative z-10"
+          />
+
+          {/* Image Container Section */}
+          <div className="story-img-container">
+            {/* Image Mask Section */}
+            <div className="story-img-mask">
+              {/* Image Content Section */}
+              <div className="story-img-content">
+                <img
+                  ref={frameRef}
+                  onMouseMove={handleMouseMove}
+                  onMouseLeave={handleMouseLeave}
+                  onMouseUp={handleMouseLeave}
+                  onMouseEnter={handleMouseLeave}
+                  src="/img/entrance.webp"
+                  alt="entrance.webp"
+                  className="object-contain"
+                />
+              </div>
+            </div>
+
+            {/* For the rounded corner effect */}
+            <svg
+              className="invisible absolute size-0"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <defs>
+                <filter id="flt_tag">
+                  <feGaussianBlur
+                    in="SourceGraphic"
+                    stdDeviation="8"
+                    result="blur"
+                  />
+                  <feColorMatrix
+                    in="blur"
+                    mode="matrix"
+                    values="1 0 0 0 0  0 1 0 0 0  0 0 1 0 0  0 0 0 19 -9"
+                    result="flt_tag"
+                  />
+                  <feComposite
+                    in="SourceGraphic"
+                    in2="flt_tag"
+                    operator="atop"
+                  />
+                </filter>
+              </defs>
+            </svg>
+          </div>
+        </div>
+
+        <div className="-mt-80 flex w-full justify-center md:-mt-64 md:me-44 md:justify-end">
+          <div className="flex h-full w-fit flex-col items-center md:items-start">
+            {/* Story Description Section */}
+            <p className="mt-3 max-w-sm text-center font-circular-web text-violet-50 md:text-start">
+              Where realms converge, lies Zentry and the boundless pillar.
+              Discover its secrets and shape your fate amidst infinite
+              opportunities.
+            </p>
+
+            {/* Button Section */}
+            <Button
+              id="realm-btn"
+              title="discover prologue"
+              containerClass="mt-5"
+            />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default FloatingImage;
